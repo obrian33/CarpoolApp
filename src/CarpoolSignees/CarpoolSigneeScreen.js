406d1a8519fb036814e3cbc8c0e2a31c9cdba5a8
O'Brian Santos <obrians@uci.edu>
2019-06-23 14:55:50 -0700

    Switch selection available

--- src/CarpoolSignees/CarpoolSigneeScreen.js
+++ src/CarpoolSignees/CarpoolSigneeScreen.js
@@ -3,17 +3,29 @@ import { View, Text, Switch } from 'react-native';
 import { styles } from '../BaseStyle';
 
 export default class CarpoolSigneeScreen extends Component {
-    availableUsers = ['O\'Brian', 'Nima', 'Danielle', 'Brian']; // Eventually be a webcall
+    availableUsers = [
+        { id: 'O\'Brian', availability: true },
+        { id: 'Nima', availability: false, },
+        { id: 'Danielle', availability: true },
+        { id: 'Brian', availability: false }]; // Eventually be a webcall
 
+    state = {
+        users: this.availableUsers
+    }
 
     render() {
         return (
             <View style={styles.container}>
                 <Text style={{ fontSize: 30, paddingLeft: 20 }}>Available Carpoolees</Text>
-                {this.availableUsers.map((users, index) => {
-                    return <View style={{flexDirection: 'row', padding: 20, justifyContent: 'space-between'}}key={index}>
-                        <Text>{users}</Text>
-                        <Switch title="Sign"></Switch>
+                {this.availableUsers.map((user, index) => {
+                    return <View style={{ flexDirection: 'row', padding: 20, justifyContent: 'space-between' }} key={index}>
+                        <Text>{user.id}</Text>
+                        <Switch value={user.availability} onValueChange={(value) => {
+                            user.availability = value;
+                            this.setState({
+                                users: this.availableUsers.filter( x => x.id !== user.id).push(user)
+                            });
+                        }} title="Sign"></Switch>
                     </View>
                 })}
             </View>
